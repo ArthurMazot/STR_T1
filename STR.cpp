94dba4bfee94c05e50f4dcf04212982a1ce379fe
ArthurMazot <arthurmazot@hotmail.com>
2025-09-09 18:57:46 -0300

    Add files via upload

--- STR.cpp
+++ STR.cpp
@@ -25,31 +25,39 @@ void print(Task *tasks, int numTasks){
 int main(){
     Task tasks[MAX];
     int numTasks;
-
     while(1){
         //Input//
         cin >> numTasks;
         if(!numTasks) break;
 
-        for(int i = 0; i < numTasks; i++)
-            cin >> tasks[i].j >> tasks[i].c >> tasks[i].p >> tasks[i].d;
+    for(int i = 0; i < numTasks; i++)
+        cin >> tasks[i].j >> tasks[i].c >> tasks[i].p >> tasks[i].d;
 
-        //Processamento//
-        int w[2] = {tasks[numTasks-1].c};
+    //print(tasks, numTasks);
+
+    //Processamento//
+    int n = 0;
+    while(n < numTasks){
+        if(tasks[n].d <= tasks[n].p){
+            n++;
+            continue;}
+
+        int w[2] = {tasks[n].c};
         int r = 0;
-        int i = 0;
+        int q = 0;
 
         do{
-            w[1] = (i+1)*tasks[numTasks-1].c;
-            for(int j = 0; j < numTasks-1; j++)
-                w[1] += ((w[0]+tasks[j].j)/tasks[j].p + ((w[0]+tasks[j].j) % tasks[j].p) ? 1 : 0)*tasks[j].c;
+            w[1] = (q+1)*tasks[n].c;
+            for(int j = 0; j < n; j++)
+                w[1] += ((w[0]+tasks[j].j)/tasks[j].p + (((w[0]+tasks[j].j) % tasks[j].p) ? 1 : 0))*tasks[j].c;
             
             w[0] = w[1];
 
-            if(r < tasks[numTasks-1].j + w[0] - i*tasks[numTasks-1].p) 
-                r = tasks[numTasks-1].j + w[0] - i*tasks[numTasks-1].p;
-        }while(w[0] > ++i*tasks[numTasks-1].p);
-    
-        //Output//
-        cout << numTasks << " " << r << " " << ((r <= tasks[numTasks-1].d) ? "YES":"NO") << endl;}
+            if(r < tasks[n].j + w[0] - q*tasks[n].p)
+                r = tasks[n].j + w[0] - q*tasks[n].p;
+
+        }while(w[0] > ++q*tasks[n].p);
+        
+       //Output//
+        cout << n+1 << " " << r << " " << ((r <= tasks[n++].d) ? "YES":"NO") << endl;}}
 }
